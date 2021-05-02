b7f99fab5cb8e90188c93073b333aa06e64361d4
dorin100 <Bica123!>
2021-05-02 18:57:39 +0300

    test1

---
+++ .buildkite/sync_tests_commit_results.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+git config pull.rebase true
+git pull origin master
+
+git add sync_tests/sync_results.db
+git add sync_tests/csv_files
+git commit -m "added sync values for"
+git push origin master
