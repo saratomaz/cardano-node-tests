ce587aee1f2bcf0167a2c5e3fd8a2845b5c8fa35
dorin100 <Bica123!>
2021-05-04 13:15:29 +0300

    test1

--- .buildkite/sync_tests_commit_results.sh
+++ .buildkite/sync_tests_commit_results.sh
@@ -5,12 +5,21 @@ echo "Authenticating push using SSH with $sshkey"
 export GIT_SSH_COMMAND="ssh -i $sshkey -F /dev/null"
 remote="git@github.com:input-output-hk/cardano-node-tests.git"
 
+echo "000000000000000000000000"
 git fetch origin
+echo "111111111111111111"
 git merge origin/dorin/sync_test1
+echo "22222222222222222222"
 git status
-git pull
+#git pull
+echo "3333333333333333333"
 
 git add sync_tests/sync_tests_results.db
 git add sync_tests/csv_files
+echo "444444444444444444444"
+
 git commit -m "added sync test values"
+echo "55555555555555555555"
+
 git push origin master
+echo "66666666666666666666666"
