f3af24bd38fe9119007feca08f52707452b0dd93
dorin100 <Bica123!>
2021-05-04 14:44:06 +0300

    test1

--- .buildkite/sync_tests_commit_results.sh
+++ .buildkite/sync_tests_commit_results.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-: "${sshkey:=/run/keys/buildkite-ssh-iohk-devops-private}"
-echo "Authenticating push using SSH with $sshkey"
-export GIT_SSH_COMMAND="ssh -i $sshkey -F /dev/null"
-remote="git@github.com:input-output-hk/cardano-node-tests.git"
+#: "${sshkey:=/run/keys/buildkite-ssh-iohk-devops-private}"
+#echo "Authenticating push using SSH with $sshkey"
+#export GIT_SSH_COMMAND="ssh -i $sshkey -F /dev/null"
+#remote="git@github.com:input-output-hk/cardano-node-tests.git"
+
+git config --global user.name "sync_tests"
+git config --global user.email "action@github.com"
 
 echo "000000000000000000000000"
 git fetch origin
