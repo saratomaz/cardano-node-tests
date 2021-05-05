b6f337ca9251758e5071e27c318f89eb6e562ec5
dorin100 <Bica123!>
2021-05-05 20:57:37 +0300

    test1

--- sync_tests/write_values_to_db.py
+++ sync_tests/write_values_to_db.py
@@ -176,7 +176,7 @@ def main():
     print("  ==== Check if there are DB columns for all the eras")
 
     print(f"Get the list of the existing eras in test")
-    eras_in_test = sync_test_results_dict["eras_in_test"].replace("[", "").replace("]", "").replace('"', '').split(",").strip()
+    eras_in_test = sync_test_results_dict["eras_in_test"].replace("[", "").replace("]", "").replace('"', '').strip().split(",")
     print(f"eras_in_test: {eras_in_test}")
 
     print(f"Get the column names inside the DB tables")
